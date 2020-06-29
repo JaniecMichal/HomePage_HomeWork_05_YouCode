fd8cc6829187ea16f89de8956dff2bdfc27a938b
Michal Janiec <michal.janiec95@gmail.com>
2020-06-29 21:10:53 +0200

    TimeScript function refactoring

--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,24 @@
-function countDown() {
-	let today = new Date();
-	let day = today.getDate();
-	let month = today.getMonth() + 1;
-	let year = today.getFullYear();
+{
+	const TimeAndDate = () => {
+		const today = new Date();
+		const day = today.getDate();
+		const month = today.getMonth() + 1;
+		const year = today.getFullYear();
 
+		let hour = today.getHours();
+		let minutes = today.getMinutes();
+		let seconds = today.getSeconds();
+		let TimerHolder = document.querySelector(".mainHeader__timer");
 
-	let hour = today.getHours();
-	if (hour < 10) hour = "0" + hour;
-	let minutes = today.getMinutes();
-	if (minutes < 10) minutes = "0" + minutes;
-	let seconds = today.getSeconds();
-	if (seconds < 10) seconds = "0" + seconds;
+		if (hour < 10) hour = "0" + hour;
+		if (minutes < 10) minutes = "0" + minutes;
+		if (seconds < 10) seconds = "0" + seconds;
 
-	document.getElementById("timer").innerHTML = day + "/" + month + "/" + year + "|" + hour + ":" + minutes + ":" + seconds;
+		TimerHolder.innerHTML = day + "/" + month + "/" + year + "|" + hour + ":" + minutes + ":" + seconds;
 
-	setTimeout("countDown()", 1000);
+		setTimeout(TimeAndDate, 1000);
+	}
+	
+	window.addEventListener("onload", TimeAndDate());
+
 }
-window.addEventListener("click", countDown());

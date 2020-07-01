83e6489caab5246278b998265bc4f521c04d2b16
Michal Janiec <michal.janiec95@gmail.com>
2020-07-01 22:31:41 +0200

    Refactorized date setting function

--- js/timer.js
+++ js/timer.js
@@ -1,14 +1,25 @@
 {
-	const TimeAndDate = () => {
+	const getDate = () => {
+
 		const today = new Date();
 		const day = today.getDate();
 		const month = today.getMonth() + 1;
 		const year = today.getFullYear();
 
-		let hour = today.getHours();
-		let minutes = today.getMinutes();
-		let seconds = today.getSeconds();
-		let TimerHolder = document.querySelector(".mainHeader__timer");
+		setDate(day, month, year);
+
+	}
+const setDate=(day,month,year)=>{
+	const dateHolder = document.querySelector(".js-dateHolder");
+	dateHolder.innerHTML = `${day}/${month}/${year}`
+}
+
+	const setTime = () => {
+
+		const hour = today.getHours();
+		const minutes = today.getMinutes();
+		const seconds = today.getSeconds();
+		const TimerHolder = document.querySelector(".mainHeader__timer");
 
 		if (hour < 10) hour = "0" + hour;
 		if (minutes < 10) minutes = "0" + minutes;
@@ -18,7 +29,7 @@
 
 		setTimeout(TimeAndDate, 1000);
 	}
-	
-	window.addEventListener("onload", TimeAndDate());
+
+	window.addEventListener("onload", getDate());
 
 }

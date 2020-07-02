28025352be89f080000cfad9536ff723488204e7
Michal Janiec <michal.janiec95@gmail.com>
2020-07-02 20:57:58 +0200

    test commit during refactorizing

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,56 +1,11 @@
 {
+    
     const Welcome = () => {
         console.log("Witajcie developerzy w Imperium Kodu");
     };
 
-    const changeQuot = document.querySelector(".Buttons__changeQuotation");
-    const SetDayQuotation = () => {
-        const today = new Date();
-        const day = today.getDay();
-        const quotText = document.querySelector(".SubHeader__quotText");
-
-        if (changeQuot.innerText == "USTAW CYTAT NA DZIŚ") {
-            switch (day) {
-                case 6:
-                case 5:
-                    quotText.innerText = '"Ponieważ więc książę obowiązany jest umieć używać bestii, powinien sobie wybrać lisa i lwa, lew bowiem nie poradzi przeciw sieciom, lis nie poradzi przeciw wilkom. Należy więc być lisem, aby się poznać na sieciach, i lwem, aby odstraszać wilków." - Niccolò Machiavelli';
-                    changeQuot.innerText = "Powróć do głównego motta strony";
-                    break;
-                case 0:
-                case 2:
-                    quotText.innerText = '"Błogosławiony Pan, Opoka moja, On moje ręce zaprawia do walki, moje palce do bitwy." - Ps 144, 1';
-                    changeQuot.innerText = "Powróć do głównego motta strony";
-                    break;
-                case 1:
-                case 3:
-                    quotText.innerText = '"Miałeś kiedyś sen, który wydawał się prawdziwy? A gdybyś nie mógł się z niego obudzić? Jak odróżniłbyś świat snu od realnego?" - Morfeusz, Matrix';
-                    changeQuot.innerText = "Powróć do głównego motta strony";
-                    break;
-                case 4:
-                    quotText.innerText = '"Jeszcze nigdy tak wielu nie zawdzięczało tak wiele tak nielicznym." - Winston Churchill';
-                    changeQuot.innerText = "Powróć do głównego motta strony";
-                    break;
-
-            }
-        } else {
-            quotText.innerText = '"To, co czynimy za życia, odbija się echem w wieczności." - Maximus Decimus Meridius';
-            changeQuot.innerText = "USTAW CYTAT NA DZIŚ";
-
-        }
-    };
-    Welcome();
-    changeQuot.addEventListener("click", SetDayQuotation);
-}
-
-
-
-
-
-
-{
-    const button = document.querySelector(".Buttons__changeColorShadow");
-
     const toggleShadowColor = () => {
+
         const content = document.querySelectorAll(".content");
         const footer = document.querySelector(".footer");
 
@@ -60,7 +15,50 @@
         footer.classList.toggle("newFooter");
     };
 
-    button.addEventListener("click", toggleShadowColor);
+    const SetDayQuotation = () => {
+      
+        const quotText = document.querySelector(".SubHeader__quotText");
+        if (changeQuotbutton.innerText === "USTAW CYTAT NA DZIŚ") quotText.innerHTML = returnDayQuotation();
+        else quotText.innerHTML = backToMainQuotation();
+    };
+
+    const returnDayQuotation = () => {
+        const today = new Date();
+        const day = today.getDay();
+
+        switch (day) {
+            case 6:
+            case 5:
+                return '"Ponieważ więc książę obowiązany jest umieć używać bestii, powinien sobie wybrać lisa i lwa, lew bowiem nie poradzi przeciw sieciom, lis nie poradzi przeciw wilkom. Należy więc być lisem, aby się poznać na sieciach, i lwem, aby odstraszać wilków." - Niccolò Machiavelli';
+            case 0:
+            case 2:
+                return '"Błogosławiony Pan, Opoka moja, On moje ręce zaprawia do walki, moje palce do bitwy." - Ps 144, 1';
+            case 1:
+            case 3:
+                return '"Miałeś kiedyś sen, który wydawał się prawdziwy? A gdybyś nie mógł się z niego obudzić? Jak odróżniłbyś świat snu od realnego?" - Morfeusz, Matrix';
+            case 4:
+                return '"Jeszcze nigdy tak wielu nie zawdzięczało tak wiele tak nielicznym." - Winston Churchill';
+        }
+    };
+
+    const backToMainQuotation = () => {
+        return `"To, co czynimy za życia, odbija się echem w wieczności." - Maximus Decimus Meridius`;
+    };
+
+
+    const init = () => {
+        const changeQuotbutton = document.querySelector(".Buttons__changeQuotation");
+        const changeShadowbutton = document.querySelector(".Buttons__changeColorShadow");
+        const textButton = changeQuotbutton.innerText
+
+        Welcome();
+
+        changeQuotbutton.addEventListener("click", SetDayQuotation);
+        changeShadowbutton.addEventListener("click", toggleShadowColor);
+    };
+
+    init();
+
 }
 
 
